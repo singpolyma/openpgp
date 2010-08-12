fa82f13fd074e7992d55e0b3ca818c4e5d34fdcb
Stephen Paul Weber <singpolyma@singpolyma.net>
2010-08-12 16:02:32 -0500

    Implement Engine::OpenSSL::RSA

--- lib/openpgp/engine/openssl.rb
+++ lib/openpgp/engine/openssl.rb
@@ -18,6 +18,88 @@ module OpenPGP
       end
 
       ##
+      # Wrap OpenSSL RSA methods for use with OpenPGP
+      class RSA
+        def initialize(packet)
+          packet = OpenPGP::Message::parse(packet) if packet.is_a?(String)
+          @key = @message = nil
+          if packet.is_a?(OpenPGP::Packet::PublicKey) || \
+            (packet.respond_to?(:first) && packet.first.is_a?(OpenPGP::Packet::PublicKey))
+            @key = packet
+          else
+            @message = packet
+          end
+        end
+
+        ##
+        # @param  [String] keyid (Optional)
+        # @return OpenPGP::Packet::PublicKey
+        def key(keyid=nil)
+          return nil unless @key
+          keyid.upcase! if keyid
+          if @key.is_a?(Enumerable) # Like an OpenPGP::Message
+            @key.select {|p| p.is_a?(OpenPGP::Packet::PublicKey) && (!keyid || \
+              p.fingerprint[keyid.length*-1,keyid.length].upcase == keyid)
+            }.first
+          end || @key
+        end
+
+        ##
+        # @param  [String] keyid (Optional)
+        # @return OpenSSL::PKey::RSA
+        def rsa_key(keyid=nil)
+          self.class.convert_key(key(keyid))
+        end
+
+        ##
+        # @param  packet message to verify with @key, or key (OpenPGP or RSA) to check @message with
+        # @param  [Integer] index specify which signature to verify (if there is more than one)
+        # @return OpenSSL::PKey::RSA
+        def verify(packet, index=0)
+          packet = OpenPGP::Message::parse(packet) if packet.is_a?(String)
+          if packet.is_a?(OpenPGP::Message) && !packet.is_a?(OpenPGP::Packet::PublicKey)
+            signature_packet, data_packet = packet.signature_and_data(index)
+            k = rsa_key(signature_packet.issuer)
+            return nil unless k && signature_packet.key_algorithm_name == 'RSA'
+            return packet.verify({'RSA' => {signature_packet.hash_algorithm_name => lambda {|m,s|
+              k.verify(signature_packet.hash_algorithm_name, s.first, m)
+            }}})
+          else
+          end
+        end
+
+        ##
+        # @param  packet
+        # @return [OpenSSL::PKey::RSA]
+        def self.convert_key(packet)
+          # packet is already an key
+          return packet if packet.is_a?(::OpenSSL::PKey::RSA)
+          unless packet.is_a?(Hash)
+            # Get the first item in a message
+            packet = packet.first if packet.is_a?(Enumerable)
+            # TODO: Error if packet.algorithm not RSA
+            packet = packet.key # Get key material
+          end
+
+          # Create blank key and fill the fields
+          key = ::OpenSSL::PKey::RSA.new
+          packet.each {|k,v|
+            if v.is_a?(Numeric)
+              v = ::OpenSSL::BN.new(v.to_s)
+            elsif !(v.is_a?(::OpenSSL::BN))
+              # Convert the byte string to an OpenSSL::BN
+              v = v.reverse.enum_for(:each_char).enum_for(:each_with_index) \
+                .inject(::OpenSSL::BN.new('0')) {|c, (b,i)|
+                  c + (b.force_encoding('binary').ord << i*8)
+                }
+            end
+            key.send("#{k}=".intern, v)
+          }
+          key
+        end
+      end
+
+      ##
       # @private
       module Random #:nodoc:
         def number(bits = 32, options = {})
