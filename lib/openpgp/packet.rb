387e3e5ea9c0916a0a316cb55682958f428942a1
devrandom <c1.github@niftybox.net>
2010-11-01 22:19:43 -0700

    convert the openpgp script into a message dumper

--- lib/openpgp/packet.rb
+++ lib/openpgp/packet.rb
@@ -503,6 +503,11 @@ module OpenPGP
         buffer.write_byte(algorithm.to_i)
         buffer.write_s2k(s2k)
       end
+
+      def to_s
+        salt = s2k.salt.unpack('H*')
+        ":symkey enc packet: version #{version}, cipher #{algorithm}, hash #{s2k.algorithm}, salt #{salt}, count #{s2k.count}"
+      end
     end
 
     ##
@@ -720,6 +725,10 @@ module OpenPGP
 
       def write_body(buffer)
         buffer.write(data)
+      end
+
+      def to_s
+        ":encrypted data packet:"
       end
     end
 
